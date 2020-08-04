ac8e9fd98f04696564572e684a44b362055a0693
djonata-dias <diasdjonata@gmail.com>
2020-08-04 14:56:12 -0300

    Updates

---
+++ precache-manifest.291732afc7bcebd3d4c7a1ec3570cf8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fee674a622f990ec9186a943c09dfb0",
+    "url": "/portifolio/index.html"
+  },
+  {
+    "revision": "a9fee31b3cce3b6dc1bc",
+    "url": "/portifolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fbad55bd498c23830931",
+    "url": "/portifolio/static/js/2.4a0e4a59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portifolio/static/js/2.4a0e4a59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9fee31b3cce3b6dc1bc",
+    "url": "/portifolio/static/js/main.60d3ccb4.chunk.js"
+  },
+  {
+    "revision": "9ad50937adffe7b02ae7",
+    "url": "/portifolio/static/js/runtime-main.4d860e73.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portifolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
