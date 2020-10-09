68a2cc30dcae5b872e4cbe52d774d88ab5011aef
djonata-dias <diasdjonata@gmail.com>
2020-10-09 20:29:02 -0300

    adicionei mais imagens no index do images

--- src/images/index.js
+++ src/images/index.js
@@ -3,11 +3,21 @@
 export { default as fotoDOT } from './fotos/fotoDOT.png';
 export { default as fotoTrybe } from './fotos/fotoTrybe.png';
 
+// Icones de linguagem de programação ou tecnologias 
+
+export { default as htmlIcon } from './icons/html-icon.png';
+export { default as vanillajsIcon } from './icons/vanillajs-icon.png';
+export { default as cssIcon } from './icons/css-icon.png';
+export { default as bootstrapIcon } from './icons/bootstrap-icon.png';
+export { default as jestLogo } from './icons/jest-icon.png';
+export { default as reactLogo } from './icons/react-icon.png';
+export { default as reactTestingLibraryLogo } from './icons/react-testing-library-icon.png';
+export { default as mysqlIcon } from './icons/mysql-icon.png';
+export { default as mongodbIcon } from './icons/mongodb-icon.png';
+export { default as netsuiteIcon } from './icons/netsuite-icon.png';
+
 // Icones
 
-export { default as reactLogo } from './icons/react-icon.png';
-export { default as jestLogo } from './icons/jest-icon.png';
-export { default as reactTestingLibraryLogo } from './icons/react-testing-library-icon.png';
 export { default as dropdownIcon } from './icons/dropdown-icon.png';
 export { default as githubIcon } from './icons/github-pages-icon.png';
 export { default as instagramIcon } from './icons/instagram-icon.png';
