dc2e825a6a7afa5f701e2252b9c7f75a11e1099c
djonata-dias <diasdjonata@gmail.com>
2020-10-09 17:22:49 -0300

    Adicionei as variaveis de exportação das imagens adicionadas

---
+++ src/images/index.js
@@ -0,0 +1,14 @@
+// Fotos
+
+export { default as fotoDOT } from './fotos/fotoDOT.png';
+export { default as fotoTrybe } from './fotos/fotoTrybe.png';
+
+// Icones
+
+export { default as reactLogo } from './icons/react-icon.png';
+export { default as jestLogo } from './icons/jest-icon.png';
+export { default as reactTestingLibraryLogo } from './icons/react-testing-library-icon.png';
+export { default as dropdownIcon } from './icons/dropdown-icon.png';
+export { default as githubIcon } from './icons/github-pages-icon.png';
+export { default as instagramIcon } from './icons/instagram-icon.png';
+export { default as likedinIcon } from './icons/linkedin-icon.png';
